8aad07944c86135914327781b09a2f3f9ee0ba26
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-02-16 01:47:54 -0700

    pagination

--- src/componets/movieCard.svelte
+++ src/componets/movieCard.svelte
@@ -4,10 +4,39 @@
   const props = {
     movie: {},
   };
+  console.log();
 </script>
 
 <div class="movie-card">
+  <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
   <h2>{movie.title}</h2>
   <p>{movie.overview}</p>
-  <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
 </div>
+
+<style>
+  .movie-card {
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    width: 300px;
+  }
+
+  .movie-card h2 {
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+  }
+
+  .movie-card p {
+    font-size: 1rem;
+    line-height: 1.5;
+    margin-bottom: 20px;
+  }
+
+  .movie-card img {
+    border-radius: 5px;
+    height: 400px;
+    object-fit: cover;
+    width: 100%;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,94 @@
-<!-- <script>
-  import axios from "axios";
-  import { onMount } from "svelte";
-  let movies = [];
-  const endpoint =
-    "https://api.themoviedb.org/3/movie/popular?api_key=88d2c735e36149b50c9d46f09826ec06&language=en-US&page=1";
-  onMount(async function () {
-    try {
-      const res = await axios.get(endpoint);
-      console.log(res.data);
-      movies = res.data.results;
-      console.log(movies);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  import MovieCard from "../componets/movieCard.svelte";
-</script>
-
-<MovieCard />
-<section /> -->
 <script>
   import axios from "axios";
   import { onMount } from "svelte";
   import MovieCard from "../componets/movieCard.svelte";
+
+  const API_KEY = "88d2c735e36149b50c9d46f09826ec06";
+  const BASE_URL = "https://api.themoviedb.org/3/movie/popular";
+  const DEFAULT_PARAMS = {
+    api_key: API_KEY,
+    language: "en-US",
+    page: 1,
+  };
+
   let movies = [];
-  const endpoint =
-    "https://api.themoviedb.org/3/movie/popular?api_key=88d2c735e36149b50c9d46f09826ec06&language=en-US&page=1";
-  onMount(async function () {
+  let totalPages = 0;
+  let currentPage = DEFAULT_PARAMS.page;
+
+  const getMovies = async (params) => {
     try {
-      const res = await axios.get(endpoint);
-      console.log(res.data);
+      const res = await axios.get(BASE_URL, { params });
       movies = res.data.results;
-      console.log(movies);
+      totalPages = res.data.total_pages;
     } catch (error) {
       console.error(error);
     }
+  };
+
+  onMount(() => {
+    getMovies(DEFAULT_PARAMS);
   });
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      getMovies({ ...DEFAULT_PARAMS, page: currentPage });
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      currentPage--;
+      getMovies({ ...DEFAULT_PARAMS, page: currentPage });
+    }
+  };
 </script>
 
-{#each movies as movie}
-  <MovieCard {movie} />
-{/each}
+<div class="movie-list">
+  {#each movies as movieData}
+    <MovieCard movie={movieData} />
+  {/each}
+</div>
+
+<div class="pagination">
+  <button on:click={handlePrevPage} disabled={currentPage === 1}>
+    Previous
+  </button>
+  <button on:click={handleNextPage} disabled={currentPage === totalPages}>
+    Next
+  </button>
+</div>
+
+<!-- App.svelte -->
+<style>
+  .movie-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+  }
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+  .pagination button {
+    background-color: white;
+    border: 1px solid gray;
+    border-radius: 5px;
+    color: black;
+    font-size: 1rem;
+    margin: 0 5px;
+    padding: 5px 10px;
+    transition: background-color 0.3s ease;
+  }
+  .pagination button:hover {
+    background-color: gray;
+    color: white;
+  }
+  .pagination button:disabled {
+    background-color: gray;
+    color: white;
+    cursor: not-allowed;
+  }
+</style>
