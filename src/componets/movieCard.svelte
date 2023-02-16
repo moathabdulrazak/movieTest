124a134c38b24f980274c7969d94f1b6b242552d
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-02-16 01:34:22 -0700

    first tine using svelte

---
+++ src/componets/movieCard.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let movie;
+  // Declare the prop that will be passed to this component
+  const props = {
+    movie: {},
+  };
+</script>
+
+<div class="movie-card">
+  <h2>{movie.title}</h2>
+  <p>{movie.overview}</p>
+  <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<!-- <script>
+  import axios from "axios";
+  import { onMount } from "svelte";
+  let movies = [];
+  const endpoint =
+    "https://api.themoviedb.org/3/movie/popular?api_key=88d2c735e36149b50c9d46f09826ec06&language=en-US&page=1";
+  onMount(async function () {
+    try {
+      const res = await axios.get(endpoint);
+      console.log(res.data);
+      movies = res.data.results;
+      console.log(movies);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  import MovieCard from "../componets/movieCard.svelte";
+</script>
+
+<MovieCard />
+<section /> -->
+<script>
+  import axios from "axios";
+  import { onMount } from "svelte";
+  import MovieCard from "../componets/movieCard.svelte";
+  let movies = [];
+  const endpoint =
+    "https://api.themoviedb.org/3/movie/popular?api_key=88d2c735e36149b50c9d46f09826ec06&language=en-US&page=1";
+  onMount(async function () {
+    try {
+      const res = await axios.get(endpoint);
+      console.log(res.data);
+      movies = res.data.results;
+      console.log(movies);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+</script>
+
+{#each movies as movie}
+  <MovieCard {movie} />
+{/each}
